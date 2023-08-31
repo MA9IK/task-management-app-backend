1f1d4b10c04350181134442abee06544fdd3591b
ivan <developerjsua@gmail.com>
2023-08-31 12:50:02 +0300

    deploy config

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ const { checkAuth } = require('./utils/checkAuth');
 const { registerValidator } = require('./validations/userValidator');
 
 app.use(express.json());
-app.use(cors({ credentials: true, origin: 'https://task-management-app-frontend.vercel.app' }));
+app.use(cors({
+  origin: 'https://task-management-app-frontend-ph8hquzdl-ivanua.vercel.app/',
+  credentials: true
+}));
 app.use(cookieParser());
 dotenv.config();
 
