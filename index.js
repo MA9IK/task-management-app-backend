f485acbc9de8e4bbeecd2594d0a2f1780043680e
ivan <developerjsua@gmail.com>
2023-08-31 12:56:48 +0300

    deploy config

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const { registerValidator } = require('./validations/userValidator');
 
 app.use(express.json());
 app.use(cors({
-  origin: '‘https://task-management-app-frontend.vercel.app',
+  origin: 'https://task-management-app-frontend.vercel.app',
   credentials: true
 }));
 app.use(cookieParser());
